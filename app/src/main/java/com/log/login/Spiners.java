c7397bd23d42f9d46bb5a80bea82b9eff10b2494
Saimonses <Simonky_6@hotmail.com>
2023-09-04 14:33:17 -0300

    login

---
+++ app/src/main/java/com/log/login/Spiners.java
@@ -0,0 +1,14 @@
+package com.log.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spiners extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spiners);
+    }
+}
